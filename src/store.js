e7a2acdeb2883ec25b293cca625cd8141bebb342
Muhammad Medani <45421893+medani123@users.noreply.github.com>
2021-05-20 02:50:52 +0800

    create order

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import thunk from "redux-thunk";
 import { productsReducer } from "./reducers/productsReducer";
 import { cartReducer } from "./reducers/cartReducer";
+import { orderReducer } from "./reducers/orderReducer";
 
 const initialState = {};
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE || compose;
@@ -9,6 +10,7 @@ const store = createStore(
   combineReducers({
     products: productsReducer,
     cart: cartReducer,
+    order: orderReducer,
   }),
   initialState,
   composeEnhancer(applyMiddleware(thunk))
